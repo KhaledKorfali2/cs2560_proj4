4701c6336dec4ff7a4f94cec77e88c0e8023283e
Khaled Korfali <46685494+KhaledKorfali2@users.noreply.github.com>
2023-03-06 16:49:06 -0800

    changed cout formating in print_table

--- cs2560_proj4/print_table.cpp
+++ cs2560_proj4/print_table.cpp
@@ -6,17 +6,21 @@ using namespace std;
 
 
 void print_table(Element** table, int size) {
+    double totalMass = 0;
     cout << "Periodic Table by K. Korfali" << endl << endl;
     cout << "Name                    Abr ANo   M";
     cout << "----------------------- --- --- ------" << endl;
 
     cout << setprecision(2) << fixed << showpoint;
     for (int i = 0; i < size; i++) {
-        cout << setw(23) <<table[i]->name << " " << setw(2) << " " << 
-                setw(3) << table[i]->nameAbbriv << " " << setw(3) << 
-                table[i]->atomicNum << " " << setw(6) << table[i]->mass << endl;
+        cout << setw(23) << table[i]->name       << " " << left;
+        cout << setw(3)  << table[i]->nameAbbriv << " " << right;
+        cout << setw(3)  << table[i]->atomicNum  << " " << left;
+        cout << setw(6)  << table[i]->mass       << right << endl;
+
+        totalMass += table[i]->mass;
     }
     cout << endl;
-    cout << "The average mass =\t\t"  << get_average_mass(table, size) << endl;
+    cout << "The average mass =\t\t"  << totalMass/size << endl;
     cout << "The number of elements =\t\t" << size << endl;
 }
